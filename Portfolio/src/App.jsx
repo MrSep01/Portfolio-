044833f2f73e21f37f3b4c783839bf11ba56bb15
sep <sep.alamouti@sepalamouti.com>
2023-12-08 04:13:51 -0800

    completed-repo

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react'
-import './App.css'
+import React, { useEffect, useState } from 'react';
+import './App.css';
 import WelcomePage from './component/WelcomePage';
 import HomePage from './component/HomePage';
 import ToggleTheme from './component/ToggleTheme';
